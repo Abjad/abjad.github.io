66f510b6429bd4713457419a863ef2965725a0a8
Trevor Bača <trevor.baca@gmail.com>
2025-10-03 11:38:29 -0400

    Update docs

---
+++ _images/lilypond-734d0b454a76657381b1845a54adeddc59c064939648397f9815b4aee1549edf.ly
@@ -0,0 +1,6 @@
+\version "2.19.83"
+\language "english"
+\score
+{
+    <b d' bf''>4
+}
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.29',
+    VERSION: '3.30',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
